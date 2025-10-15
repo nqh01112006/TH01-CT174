c642ea8c47708803b3b6f06235808f7bc8e08370
nqh01112006 <quynhhuongcanglong@gmail.com>
2025-10-15 09:58:31 +0700

    Update TH1.cpp

--- PTvaTKTT/TH1.cpp
+++ PTvaTKTT/TH1.cpp
@@ -75,6 +75,14 @@ void InsertSort(recordtype a[], int n)
 }
 
 // BubleSort
+void BubleSort(recordtype a[],int n){
+	int i,j;
+	for(i=0;i<=n-1;i++){
+		for(j=n-1;j>i;j--){
+			if(a[j-1].key>a[j].key) Swap(a[j-1],a[j]);
+		}
+	}
+}
 
 //Ham main
 main(){
@@ -93,4 +101,5 @@ main(){
 	return 0;
 	
 }
+
 
